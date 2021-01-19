8aa1d6ed3bf9211e1dce07c33b47fabbe1be1c39
nyan9 <rynnaing@gmail.com>
2021-01-19 14:14:10 -0500

    created ::Employee|::Manager

---
+++ class_inheritance.rb
@@ -0,0 +1,20 @@
+class Employee
+  def initialize(name, title, salary, boss)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
+
+  def bonus(multiplier)
+    bonus = @salary * multiplier
+  end
+end
+
+class Manager < Employee
+
+  attr_writer :employees
+  def initialize
+    @employees = []
+  end
+end
