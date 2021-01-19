704c60557f225366294ea408bd6a95f123d72b4c
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-01-19 16:34:28 -0500

    populated grid, added move_peice, valid_pos

--- chess/board.rb
+++ chess/board.rb
@@ -1,8 +1,19 @@
 class Board
+
   def initialize
-    @rows = Array.new(8) {Array.new(8)}
+    @rows = Array.new(8) {Array.new(8, "_")}
     @piece = Piece.new(color, board, pos)
     @null_piece = Piece.new(color, board, pos)
+
+    (0...@rows.length).each do |row| 
+      (0...@rows.length).each do |col|
+        if row <= 1 || row >= 6
+          @rows[row][col] = @piece
+        else 
+          @rows[row][col] = @null_piece
+        end
+      end
+    end
   end
   
   def [](pos)
@@ -14,13 +25,24 @@ class Board
   end
 
   def move_piece(start_pos, end_pos)
-    unless valid_pos?(end_pos) || @rows[start_pos] == @null_piece
+    if !valid_pos?(end_pos) || @rows[start_pos] == @null_piece
       raise "Please enter a valid position for your piece!"
+    else
+      @rows[end_pos]= @rows[start_pos]
+      @rows[start_pos]= @null_piece
     end
   end
 
+  def valid_pos?(pos)
+    if @rows[pos] == @null_piece || @row[pos] == @piece
+      return true
+    end
+
+    false
+  end
+
   private
-  @null_piece = NullPiece.new()
+  @null_piece = NullPiece.new
 end
 
 board1 = Board.new
