709a59e59e11868c3785bdf6e282ca067906061f
nyan9 <rynnaing@gmail.com>
2021-01-19 17:35:24 -0500

    modified/tested Board#move_piece + Board#valid_pos? + populated Board#initialize with ::Pieces + ::NullPieces

--- chess/board.rb
+++ chess/board.rb
@@ -1,16 +1,19 @@
-class Board
+require_relative "./piece.rb"
 
+class Board
+  # attr_accessor :rows, :piece, :null_piece
+  
   def initialize
     @rows = Array.new(8) {Array.new(8, "_")}
-    @piece = Piece.new(color, board, pos)
-    @null_piece = Piece.new(color, board, pos)
+    @piece = Piece.new
+    @null_piece = NullPiece.new
 
     (0...@rows.length).each do |row| 
       (0...@rows.length).each do |col|
         if row <= 1 || row >= 6
-          @rows[row][col] = @piece
+          @rows[row][col] = Piece.new
         else 
-          @rows[row][col] = @null_piece
+          @rows[row][col] = NullPiece.new
         end
       end
     end
@@ -25,24 +28,30 @@ class Board
   end
 
   def move_piece(start_pos, end_pos)
-    if !valid_pos?(end_pos) || @rows[start_pos] == @null_piece
+    if !valid_pos?(end_pos) || self[start_pos] == @null_piece.class
       raise "Please enter a valid position for your piece!"
     else
-      @rows[end_pos]= @rows[start_pos]
-      @rows[start_pos]= @null_piece
+      self[end_pos]= self[start_pos]
+      self[start_pos]= NullPiece.new
     end
   end
 
   def valid_pos?(pos)
-    if @rows[pos] == @null_piece || @row[pos] == @piece
-      return true
+    if pos.first < 0 || pos.first > 7
+      return false
+    elsif pos.last < 0 || pos.last > 7
+      return false
+    end 
+
+    unless self[pos].class == @null_piece.class || self[pos].class == @piece.class
+      return false
     end
 
-    false
+    true
   end
 
-  private
-  @null_piece = NullPiece.new
+  # private
+  # @null_piece = NullPiece.new
 end
 
-board1 = Board.new
+# board1 = Board.new
--- chess/piece.rb
+++ chess/piece.rb
@@ -1,7 +1,11 @@
 class Piece
-  def initialize(color, board, pos)
-    @color = color
-    @board = board
-    @pos = pos
-  end
+  # def initialize(color, board, pos)
+  #   @color = color
+  #   @board = board
+  #   @pos = pos
+  # end
+end
+
+class NullPiece < Piece
+
 end
